14a487f852e11d9aa1a21d2d8b06de2bfc9b7fd8
ed <vercaemereduard@gmail.com>
2024-04-01 18:06:02 -0600

    read and print file

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,23 @@
 const std = @import("std");
 
+const MAX_FILE_SIZE = 1_000_000;
+
+const Keyword = enum {};
+
+const Literal = union(enum) {};
+
+const Token = union(enum) {
+    identifier: *const []u8,
+    keyword: Keyword,
+    literal: Literal,
+    comment: *const []u8,
+};
+
 pub fn main() anyerror!void {
-    std.debug.print("hello, there\n", .{});
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+    const file = try std.fs.cwd().readFileAlloc(allocator, "README.md", MAX_FILE_SIZE);
+    std.debug.print("file: {s}\n", .{file});
 }
