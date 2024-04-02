1c44703f61b2d5d093710a5b72d29f820d018380
ed <vercaemereduard@gmail.com>
2024-04-01 20:45:00 -0600

    add keywords, ops, literals, token

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,69 @@ const std = @import("std");
 
 const MAX_FILE_SIZE = 1_000_000;
 
+const Keyword = enum {
+    k_break,
+    k_dec,
+    k_do,
+    k_else,
+    k_elseif,
+    k_false,
+    k_if,
+    k_inc,
+    k_return,
+    k_true,
+    k_var,
+    k_while,
+};
+
+const Operator = enum {
+    o_comma,
+    o_semi,
+    o_oparen,
+    o_cparen,
+    o_ocurly,
+    o_ccurly,
+    o_obrack,
+    o_cbrack,
+    o_assign,
+    o_and,
+    o_pipes,
+    o_xor,
+    o_equal,
+    o_nequal,
+    o_lt,
+    o_ltequal,
+    o_gt,
+    o_gtequal,
+    o_plus,
+    o_minus,
+    o_mul,
+    o_div,
+    o_mod,
+    o_not,
+};
+
+const Literal = union(enum) {
+    boolean: bool,
+    integer: i32,
+    // TODO: multi-byte unicode characters ???
+    character: u8,
+    string: []const u8,
+};
+
+const Token = struct {
+    line: u32,
+    start: usize,
+    end: usize,
+    value: union(enum) {
+        symbol: []const u8,
+        keyword: Keyword,
+        literal: Literal,
+        operator: Operator,
+        comment: []const u8,
+    },
+};
+
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
