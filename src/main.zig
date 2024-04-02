f11667de8166f84484a783999ce213d4482c1ed7
ed <vercaemereduard@gmail.com>
2024-04-01 18:16:42 -0600

    add keywords and literals

--- src/main.zig
+++ src/main.zig
@@ -2,15 +2,33 @@ const std = @import("std");
 
 const MAX_FILE_SIZE = 1_000_000;
 
-const Keyword = enum {};
+const Keyword = enum {
+    k_break,
+    k_elseif,
+    k_return,
+    k_dec,
+    k_false,
+    k_true,
+    k_do,
+    k_if,
+    k_var,
+    k_else,
+    k_inc,
+    k_while,
+};
 
-const Literal = union(enum) {};
+const Literal = union(enum) {
+    boolean: bool,
+    integer: i32,
+    character: []const u8,
+    string: []const u8,
+};
 
 const Token = union(enum) {
-    identifier: *const []u8,
+    identifier: []const u8,
     keyword: Keyword,
     literal: Literal,
-    comment: *const []u8,
+    comment: []const u8,
 };
 
 pub fn main() anyerror!void {
