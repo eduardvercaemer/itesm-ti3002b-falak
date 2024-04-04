fdedbd8b08bef0916b66d217ce8b7de10f328783
ed <vercaemereduard@gmail.com>
2024-04-04 14:43:34 -0600

    add stub expression enum

--- src/parser.zig
+++ src/parser.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const Context = @import("context.zig").Context;
+const Literal = @import("lexer.zig").Literal;
 
 pub const ParserError = error{};
 
@@ -26,6 +27,11 @@ pub const ExprWithBlock = struct {
     stmts: std.ArrayList(Stmt),
 };
 
+pub const FunctionCall = struct {
+    function: Sym,
+    args: std.ArrayList(Expr),
+};
+
 pub const Stmt = union(enum) {
     assign: struct {
         location: Sym,
@@ -33,10 +39,7 @@ pub const Stmt = union(enum) {
     },
     inc: Sym,
     dec: Sym,
-    call: struct {
-        function: Sym,
-        args: std.ArrayList(Expr),
-    },
+    call: FunctionCall,
     if_: struct {
         expr: Expr,
         stmts: std.ArrayList(Stmt),
@@ -50,7 +53,39 @@ pub const Stmt = union(enum) {
     empty: void,
 };
 
-pub const Expr = struct {};
+pub const BinaryOp = struct {
+    left: Expr,
+    right: Expr,
+};
+
+pub const Expr = union(enum) {
+    or_: BinaryOp,
+    and_: BinaryOp,
+    comp: BinaryOp,
+    rel: struct {
+        op: BinaryOp,
+        kind: enum {
+            lt,
+            lte,
+            gt,
+            gte,
+        },
+    },
+    add: BinaryOp,
+    mul: BinaryOp,
+    unary: struct {
+        expr: Expr,
+        kind: enum {
+            plus,
+            neg,
+            not,
+        },
+    },
+    ref: Sym,
+    call: FunctionCall,
+    array: std.ArrayList(Expr),
+    literal: Literal,
+};
 
 pub fn parser(ctx: *Context) ParserError!void {
     _ = ctx;
