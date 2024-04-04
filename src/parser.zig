0976f98ad91039a25337b66c31cf256903234041
ed <vercaemereduard@gmail.com>
2024-04-04 14:20:37 -0600

    add basic definition and statements nodes

--- src/parser.zig
+++ src/parser.zig
@@ -3,6 +3,55 @@ const Context = @import("context.zig").Context;
 
 pub const ParserError = error{};
 
+pub const Program = std.ArrayList(Definition);
+
+pub const Definition = union(enum) {
+    function: Function,
+    globals: std.ArrayList(Sym),
+};
+
+pub const Function = struct {
+    symbol_id: usize,
+    params: std.ArrayList(Sym),
+    vars: std.ArrayList(Sym),
+    stmts: std.ArrayList(Stmt),
+};
+
+pub const Sym = struct {
+    symbol_id: usize,
+};
+
+pub const ExprWithBlock = struct {
+    expr: Expr,
+    stmts: std.ArrayList(Stmt),
+};
+
+pub const Stmt = union(enum) {
+    assign: struct {
+        location: Sym,
+        expr: Expr,
+    },
+    inc: Sym,
+    dec: Sym,
+    call: struct {
+        function: Sym,
+        args: std.ArrayList(Expr),
+    },
+    if_: struct {
+        expr: Expr,
+        stmts: std.ArrayList(Stmt),
+        else_if: ?std.ArrayList(ExprWithBlock),
+        else_: ?ExprWithBlock,
+    },
+    while_: ExprWithBlock,
+    do_while: ExprWithBlock,
+    break_: void,
+    return_: Expr,
+    empty: void,
+};
+
+pub const Expr = struct {};
+
 pub fn parser(ctx: *Context) ParserError!void {
     _ = ctx;
 }
